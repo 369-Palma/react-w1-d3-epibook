657de70eef76241ca084cfe603c5c546508d8fb6
Palma Iacobelli <118762347+369-Palma@users.noreply.github.com>
2023-02-08 18:17:05 +0100

    .

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Welcome from "./components/Jumbotron";
 /* import AllTheBooks from "./components/AllTheBooks"; */
 import SingleBook from "./components/SingleBook.jsx";
 import BookList from "./components/BookList";
+import book from "./components/SingleBook";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Welcome size="2" bg="blue" color="white"></Welcome>
       {/* <AllTheBooks></AllTheBooks> */}
       <SingleBook
+        book={book}
         title="The Last Wish: Introducing the Witcher"
         copertina="https://images-na.ssl-images-amazon.com/images/I/51eHtkVLL5L.jpg"
       ></SingleBook>
